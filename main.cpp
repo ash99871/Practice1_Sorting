dcd331fdf189968da2ee016f77e466cc10241f51
Maybeth2008 <lopezbedoya.2008@gmail.com>
2026-03-15 19:06:17 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,30 +7,24 @@
 using namespace std;
 
 // Funcion que divide el vector alrededor del pivote
-int dividir(vector<string>& palabras, int inicio, int fin)
-{
+int dividir(vector<string>& palabras, int inicio, int fin){
     string pivote = palabras[fin];
     int i = inicio - 1;
 
-    for(int j = inicio; j < fin; j++)
-    {
-        if(palabras[j] < pivote)
-        {
+    for(int j = inicio; j < fin; j++){
+        if(palabras[j] < pivote){
             i++;
             swap(palabras[i], palabras[j]);
         }
     }
-
     swap(palabras[i + 1], palabras[fin]);
 
     return i + 1;
 }
 
 // Funcion para el QuickSort
-void ordenarQuick(vector<string>& palabras, int inicio, int fin)
-{
-    if(inicio < fin)
-    {
+void ordenarQuick(vector<string>& palabras, int inicio, int fin){
+    if(inicio < fin){
         int posicionPivote = dividir(palabras, inicio, fin);
 
         ordenarQuick(palabras, inicio, posicionPivote - 1);
@@ -47,27 +41,21 @@ int main()
         cout << "Error al abrir el archivo" << endl;
         return 1;
     }
-
     vector<string> palabras(100000);
-
-    for(int i = 0; i < 100000; i++)
-    {
+    
+    for(int i = 0; i < 100000; i++){
         getline(archivo, palabras[i]);
     }
-
-    cout << "Dataset cargado" << endl;
+    cout << "Archivo cargado" << endl;
 
     // Desordenar palabras
     random_device rd;
     mt19937 g(rd());
-
     shuffle(palabras.begin(), palabras.end(), g);
-
-    cout << "Dataset desordenado" << endl;
+    cout << "Archivo desordenado" << endl;
 
     // Ejecutar QuickSort
     ordenarQuick(palabras, 0, palabras.size() - 1);
-
     cout << "QuickSort terminado" << endl;
 
     // Mostrar algunas palabras de prueba
