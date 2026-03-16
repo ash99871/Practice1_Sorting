b8ff2e2ace9c8b2ea21d71007dd88e8907ba30b8
Maybeth2008 <lopezbedoya.2008@gmail.com>
2026-03-15 19:01:21 -0500

    QuickSort
    Implement QuickSort algorithm to sort a list of words from a file.

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
+using namespace std;
+
+// Funcion que divide el vector alrededor del pivote
+int dividir(vector<string>& palabras, int inicio, int fin)
+{
+    string pivote = palabras[fin];
+    int i = inicio - 1;
+
+    for(int j = inicio; j < fin; j++)
+    {
+        if(palabras[j] < pivote)
+        {
+            i++;
+            swap(palabras[i], palabras[j]);
+        }
+    }
+
+    swap(palabras[i + 1], palabras[fin]);
+
+    return i + 1;
+}
+
+// Funcion para el QuickSort
+void ordenarQuick(vector<string>& palabras, int inicio, int fin)
+{
+    if(inicio < fin)
+    {
+        int posicionPivote = dividir(palabras, inicio, fin);
+
+        ordenarQuick(palabras, inicio, posicionPivote - 1);
+        ordenarQuick(palabras, posicionPivote + 1, fin);
+    }
+}
+
+int main()
+{
+    // Leer el archivo palabra por palabra
+    ifstream archivo("words_alpha.txt");
+
+    vector<string> palabras(100000);
+
+    for(int i = 0; i < 100000; i++)
+    {
+        getline(archivo, palabras[i]);
+    }
+
+    cout << "Dataset cargado" << endl;
+
+    // Desordenar palabras
+    random_device rd;
+    mt19937 g(rd());
+
+    shuffle(palabras.begin(), palabras.end(), g);
+
+    cout << "Dataset desordenado" << endl;
+
+    // Ejecutar QuickSort
+    ordenarQuick(palabras, 0, palabras.size() - 1);
+
+    cout << "QuickSort terminado" << endl;
+
+    // Mostrar algunas palabras de prueba
+    for(int i = 0; i < 50; i++)
+    {
+        cout << palabras[i] << endl;
+    }
+
+    return 0;
+}
